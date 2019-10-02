c29db3a67e9461fc6f75f48988e85886f449f6d2
Harvey Sanders <sanders.harvey@gmail.com>
2019-10-02 16:44:19 -0500

    Update React, ReactDOM, PropTypes to latest

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -11,7 +11,7 @@ const VideoList = () => (
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired,
+  videos: PropTypes.array.isRequired,
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
