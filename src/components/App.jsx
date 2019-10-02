a0185469210d4add10fa64ede3672849acde3c00
thomasmudge <alephcult@gmail.com>
2019-10-02 18:08:52 -0500

    Refactored some of App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,55 @@
-const App = () => (
-  <div>
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      videoList: [], // array?
+      currentVideo: null // this might be right
+    };
+
+    // probably have to put something else here using decontruction
+  }
+  
+  changeVideo(video){
+    // set state
+  }
+
+  updateVideoList(videos){
+    // set state
+  }
+
+  render(){
+    // probably some kind of if then...  
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            {/* put search here */}
+          </div>
+        </nav>
+      <div>Loading...</div>
+      </div>
+    );
+  }
+  
+  return ( <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        // update with new search function
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        // update with new video player
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        // update with new video list
       </div>
     </div>
   </div>
-);
+  );
+
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
