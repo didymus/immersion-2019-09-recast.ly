6a742ecb4a7825e96e50aef679980633372d0ab7
thomasmudge <alephcult@gmail.com>
2019-10-02 18:22:56 -0500

    'GET' request added in searchYouTube.js

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,13 @@
-const searchYouTube = (options, callback) => {
-  // TODO
+const searchYouTube = ({key = YOUTUBE_API_KEY, query = '', max = 5}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: key,
+    q: query,
+    maxResults: max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  }).done(data => callback(data.items))
+    .fail(err => console.log('GET failed', err));
 };
 
 window.searchYouTube = searchYouTube;
