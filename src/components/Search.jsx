1ab735565d7a0b7d6d43010c63f958f34d7a8cf0
thomasmudge <alephcult@gmail.com>
2019-10-03 11:23:48 -0500

    Added preventDefault and stopPropagation to searchVideo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,13 +6,15 @@ class Search extends React.Component {
     };
   }
 
-  searchVideos(event){
+  searchVideo(event){
+    event.preventDefault();
+    event.stopPropagation(); 
     window.searchYouTube({query: this.state.term}, this.props.searchCallback);
   }
 
   render(){
     return (
-  <form className="search-bar form-inline" onSubmit={this.searchVideos.bind(this)}>
+  <form className="search-bar form-inline" onSubmit={this.searchVideo.bind(this)}>
     <input className="form-control" type="text" 
     onChange={event => this.setState({term: event.target.value})} />
     <button className="btn hidden-sm-down" type="submit">
